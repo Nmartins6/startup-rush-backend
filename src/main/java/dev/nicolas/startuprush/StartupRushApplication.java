2ac3308a13bf175d7df40fc1bce98d0986091f82
Nicolas Martins <nicolas.edm7@gmail.com>
2025-04-17 07:08:13 -0300

    initial commit

---
+++ src/main/java/dev/nicolas/startuprush/StartupRushApplication.java
@@ -0,0 +1,13 @@
+package dev.nicolas.startuprush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupRushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupRushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nicolas/startuprush/StartupRushApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nicolas.startuprush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupRushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
